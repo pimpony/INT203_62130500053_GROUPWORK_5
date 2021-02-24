3624a3e4ffe54a52dbe375e183ba249e41ac2f6b
Paweena Thongyan <paweena.thongyan@mail.kmutt.ac.th>
2021-02-24 21:42:49 +0700

    change

--- components/PhotoItem.js
+++ components/PhotoItem.js
@@ -19,19 +19,19 @@ app.component('photo-item', {
   template:
     /*html*/`
     <div v-if="this.$emit('photoitem') !== 0"class=" ">
-        <li v-for="(photo, index) in photoitem" class="flex flex-col items-center w-full mt-5">
+        <li v-for="(gallery, index) in photoitem" class="flex flex-col items-center w-full mt-5">
           
           <div class="w-full text-gray-700 font-san flex  w-48 flex justify-between item-center">
-          <button @click="this.$emit('zoomphoto', index)"><img :src="photo.src" class=" w-full rounded-sm"
-              :alt="photo.title"></button>
+          <button @click="this.$emit('zoomphoto', index)"><img :src="gallery.src" class=" w-full rounded-sm"
+              :alt="gallery.title"></button>
           <div class="flex flex-row justify-start space-x-1">
             <button class="bg-pink-400 text-white  items-center font-semibold px-3 my-auto mx-3  rounded-md focus:outline-none" @click="this.$emit('fav', index)" >
                LIKE  
-              <span class="material-icons text-white text-xl ml-1"  v-if="photo.like > 0"> favorite</span>
+              <span class="material-icons text-white text-xl ml-1"  v-if="gallery.like > 0"> favorite</span>
             </button>
             <div class=" w-64">
-                <p class="text-lg  text-pink-400 flex justify-center item-center">{{ photo.title }}</p>
-                <p class="text-lg ">{{ photo.detail }}</p>
+                <p class="text-lg  text-pink-400 flex justify-center item-center">{{ gallery.title }}</p>
+                <p class="text-lg ">{{ gallery.detail }}</p>
               </div>
             
 
